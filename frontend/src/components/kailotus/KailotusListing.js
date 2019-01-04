c0bbd5311bd393b185f527361c13c7b1a65f5fb7
Sami Kallio <dehyd9@gmail.com>
2019-01-04 19:21:38 +0200

    1: read kailotusdata from array

--- frontend/src/components/kailotus/KailotusListing.js
+++ frontend/src/components/kailotus/KailotusListing.js
@@ -3,33 +3,32 @@ import Kailotus from './Kailotus';
 
 class KailotusListing extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.initialState = {
-      // this should load kailotus from backend
-    }
-
-    this.state = this.initialState;
-  }
-
-  handleSubmit = kailotusData => {
-    this.setState({
-
-    });
-  }
+  state = {
+    arrayOfKailotus: []
+  };
 
   render() {
     const { kailotusData } = this.state;
+    const builtKailotusArray = this.getBuiltKailotusArray();
     return (
       <div className="tile is-ancestor is-vertical">
-        <div className="columns">
-          <div className="column">
-            <Kailotus kailotusData={kailotusData}/>
-          </div>
-        </div>
+        {builtKailotusArray}
       </div>
     );
+  }
+
+  getBuiltKailotusArray() {
+    let kailotusArray = []
+    if (kailotusArray.length === 0) {
+      kailotusArray.push(
+        <div className="columns">
+          <div className="column">
+            <Kailotus kailotusData="Ei vielä kailotuksia!" />
+          </div>
+        </div>
+      );
+    }
+    return kailotusArray;
   }
 }
 
